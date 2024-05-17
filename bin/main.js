26294896abd15af78480efa0748dbfd0b936f9a4
shoaibalisher1 <166815957+shoaibalisher1@users.noreply.github.com>
2024-05-18 02:18:22 +0500

    changes done

--- bin/main.js
+++ bin/main.js
@@ -1,3 +1,5 @@
+#! /usr/bin/env node
+// SHABANG
 import inquirer from 'inquirer';
 // todos array
 // function
@@ -20,5 +22,7 @@ while (condition) {
     ]);
     todos.push(todoQuestions.firstQuestion);
     console.log(todos);
+    // Loop is running based on the below condition
     condition = (todoQuestions.secondQuestion);
 }
+//Home Work Read, update, delete and add features to be added in the to do list
